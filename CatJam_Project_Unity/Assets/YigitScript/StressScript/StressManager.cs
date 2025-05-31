d5b4ea92ee1ac48937414b584146cbf6d6e2ef54
Yiğit Şahin <yigitsahin564@gmail.com>
2025-05-31 03:54:03 +0300

    npc check 2

---
+++ CatJam_Project_Unity/Assets/YigitScript/StressScript/StressManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StressManager : MonoBehaviour
+{
+    [SerializeField] private float stressLevel = 0f;
+    [SerializeField] private float maxDistance = 2f;
+    [SerializeField] private LayerMask npcLayer;
+    [SerializeField] private GameObject buttonPanel;
+
+    void Start()
+    {
+        //buttonPanel.SetActive(false);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, maxDistance);
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * maxDistance);
+    }
+
+    void Update()
+    {
+        CheckNPC();
+    }
+
+    private void CheckNPC()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, maxDistance, npcLayer);
+
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.CompareTag("NPC"))
+            {
+                //buttonPanel.SetActive(true);
+                Debug.Log("NPC detected" );
+                return;
+            }
+        }
+
+        //Ray ray;
+
+        //ray = new Ray(transform.position, transform.forward);
+
+        //if (Physics.Raycast(ray, out RaycastHit hit, maxDistance, npcLayer))
+        //{
+        //    //buttonPanel.SetActive(true);
+        //    Debug.Log("Raycast hit");
+        //}
+    }
+}
